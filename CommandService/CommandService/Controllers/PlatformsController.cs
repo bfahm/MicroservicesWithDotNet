3f547f1386681415e84164658c7097e81475d8c2
Paula Fahmy <paula.fahmy@flairstech.com>
2021-09-18 14:25:08 +0200

    Initialized commands service

---
+++ CommandService/CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controler");
+        }
+    }
+}
